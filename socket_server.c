088f1f711d9459cdaaccbd8077e38024165c0d50
Lucas Malaman de Oliveira <malamanlucas@gmail.com>
2022-02-13 04:06:20 -0300

    x

--- socket_server.c
+++ socket_server.c
@@ -188,6 +188,7 @@ int main(int argc, char **argv){
 	while(1){
 		socklen_t clilen = sizeof(cli_addr);
 		connfd = accept(listenfd, (struct sockaddr*)&cli_addr, &clilen);
+		printf("Chegou client\n");
 
 		/* Check if max clients is reached */
 		if((cli_count + 1) == MAX_CLIENTS){
